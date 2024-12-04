8219d92a1b63aea0d496d7074ca91c0e7306b70d
musearcosu <113990819+musearcosu@users.noreply.github.com>
2024-12-04 05:14:47 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ types/index.ts
@@ -0,0 +1,95 @@
+export type DataProps = {
+  data: any
+}
+
+export type HandleProps = {
+  handle: () => any
+  args: string
+}
+
+export type ImageServerHandleProps = {
+  handle: (pageNum: number, tag: string) => any
+  args: string
+  totalHandle: (tag: string) => any
+}
+
+export type ImageHandleProps = {
+  handle: (pageNum: number, tag: string) => any
+  args: string
+  album: string
+  totalHandle: (tag: string) => any
+}
+
+export type LinkProps = {
+  handle: () => any
+  args: string
+  data: any
+}
+
+export type AlbumType = {
+  id: string;
+  name: string;
+  album_value: string;
+  detail: string;
+  show: number;
+  sort: number;
+}
+
+export type ExifType = {
+  make: any;
+  model: any;
+  bits: any;
+  data_time: any;
+  exposure_time: any;
+  f_number: any;
+  exposure_program: any;
+  iso_speed_rating: any;
+  focal_length: any;
+  lens_specification: any;
+  lens_model: any;
+  exposure_mode: any;
+  cfa_pattern: any;
+  color_space: any;
+  white_balance: any;
+}
+
+export type ImageType = {
+  id: string;
+  title: string;
+  url: string;
+  preview_url: string;
+  video_url: string;
+  exif: ExifType;
+  labels: any;
+  width: number;
+  height: number;
+  lon: string;
+  lat: string;
+  album: string;
+  detail: string;
+  type: number;
+  show: number;
+  sort: number;
+  album_name: string;
+  album_value: string;
+  copyrights: any[];
+}
+
+export type CopyrightType = {
+  id: string;
+  name: string;
+  social_name: string;
+  type: string;
+  url: string;
+  avatar_url: string;
+  detail: string;
+  default: number;
+  show: number;
+}
+
+export type Config = {
+  id: string;
+  config_key: string;
+  config_value: string;
+  detail: string;
+}
